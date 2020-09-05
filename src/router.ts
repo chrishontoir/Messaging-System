8233e242090ac2e8178e31a17472b87e33a4d92d
chrishontoir <chrishontoir@gmail.com>
2020-09-05 18:31:50 +0100

    fix: file cleanup

--- src/router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "./deps.ts";
-
-export const router = new Router();
-
-router.get("/", (ctx: any) => {
-  ctx.response.body = "Hi there";
-});
--- test/unit/app.test.ts
+++
@@ -1,9 +0,0 @@
-import { assertStrictEquals } from "https://deno.land/std@0.67.0/testing/asserts.ts";
-
-Deno.test({
-    name: 'app()',
-    fn: () => {
-        const x = 1 + 2;
-        assertStrictEquals(x, 3);
-    }
-});
